9bdce1a5a4c1580cab75938d589631a0d9bd4051
Matheus Felipe V. de Freitas <sql1freitas@gmail.com>
2024-01-08 21:15:11 -0300

    Criação do método salvar no ImagesController

--- src/main/java/io/github/sql1freitas/MyImgApi/controller/ImagesController.java
+++ src/main/java/io/github/sql1freitas/MyImgApi/controller/ImagesController.java
@@ -1,10 +1,27 @@
 package io.github.sql1freitas.MyImgApi.controller;
 
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
-@RequestMapping("/vi/images")
+@RequestMapping("/v1/images")
+@Slf4j
 public class ImagesController {
+    @PostMapping
+    public ResponseEntity save (@RequestParam("file")MultipartFile file,
+                                @RequestParam("name") String name,
+                                @RequestParam("tags")List<String> tags) {
+
+        log.info("Imagem recebida: name: {}, size {}", file.getName(), file.getSize());
+        log.info("Nome definino para a imagem: {} ", name);
+        log.info("Tags: {} ", tags);
+
+        return ResponseEntity.ok().build();
+
+
+    }
 }
