833a4818f00ece45160b21aa9f7a8ad6dff34219
Matheus Felipe V. de Freitas <sql1freitas@gmail.com>
2024-01-12 00:00:20 -0300

    Criação da classe DTO para image e adicionado método ImageToDTO

---
+++ src/main/java/io/github/sql1freitas/MyImgApi/Mapper/ImageDTO.java
@@ -0,0 +1,18 @@
+package io.github.sql1freitas.MyImgApi.Mapper;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+public class ImageDTO {
+
+    private String url;
+    private String name;
+    private String extension;
+    private Long size;
+    private LocalDate uploadDate;
+
+}
