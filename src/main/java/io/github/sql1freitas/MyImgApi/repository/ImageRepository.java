94386386b57f2bd93b9bef3d593999e347136a72
Matheus Felipe V. de Freitas <sql1freitas@gmail.com>
2024-01-12 00:07:24 -0300

    Adicionada nova extensão para o repositório e método para consultas de imagens

--- src/main/java/io/github/sql1freitas/MyImgApi/repository/ImageRepository.java
+++ src/main/java/io/github/sql1freitas/MyImgApi/repository/ImageRepository.java
@@ -1,9 +1,20 @@
 package io.github.sql1freitas.MyImgApi.repository;
 
 import io.github.sql1freitas.MyImgApi.entity.Image;
+import io.github.sql1freitas.MyImgApi.enums.ImageExtension;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
-public interface ImageRepository extends JpaRepository<Image, String> {
+public interface ImageRepository extends JpaRepository<Image, String> , JpaSpecificationExecutor<Image> {
+
+    default List<Image> findByExtensionAndNameOrTagsLike(ImageExtension extension, String query){
+        if(extension == null){
+            //TODO: add in query
+        }
+        return findAll();
+    }
 }
